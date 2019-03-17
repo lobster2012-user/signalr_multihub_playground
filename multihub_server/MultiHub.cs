26c5976c2d2d1c5ffbfff198a23dc4a271719128
lobster2012-user <lobster2012.user@gmail.com>
2019-03-17 22:00:51 +0300

    simplification

--- multihub_server/MultiHub.cs
+++ multihub_server/MultiHub.cs
@@ -13,25 +13,18 @@ namespace multihub_server
     /// </summary>
     public class MultiHub : MultiHubBase<IMultiClient>
     {
-        public MultiHub(ILogger<MultiHub> logger, IServiceProvider provider) 
-            : base(logger, provider,  new[] { typeof(Hello1Hub), typeof(Hello2Hub)})
-        {
-        }
-
-        [HubMethodName("Hello1Hub_OnMessage")]
-        public void Hello1Hub_OnMessage(string text)
-        {
-            Hello1Hub.OnMessage(text);
-        }
-        [HubMethodName("Hello2Hub_OnMessage")]
-        public void Hello2Hub_OnMessage(string text)
-        {
-            Hello2Hub.OnMessage(text);
-        }
-
         private Hello1Hub Hello1Hub => CreateHub<Hello1Hub, IHello1Client>();
         private Hello2Hub Hello2Hub => CreateHub<Hello2Hub, IHello2Client>();
 
-    }
+        public MultiHub(ILogger<MultiHub> logger, IServiceProvider provider)
+            : base(logger, provider, new[] { typeof(Hello1Hub), typeof(Hello2Hub) })
+        {
+        }
 
+        [HubMethodName(nameof(Hello1Hub_OnMessage))]
+        public void Hello1Hub_OnMessage(string text) => Hello1Hub.OnMessage(text);
+
+        [HubMethodName(nameof(Hello2Hub_OnMessage))]
+        public void Hello2Hub_OnMessage(string text) => Hello2Hub.OnMessage(text);
+    }
 }
